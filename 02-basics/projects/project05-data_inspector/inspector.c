f7eb9121ce3e4e98cdba5c6ed1ef4765a6206f87
fiberlabs <fiberlabs@protonmail.com>
2026-06-02 18:08:30 -0400

    Project 05 Complete: Data Inspector.

--- 02-basics/projects/project05-data_inspector/inspector.c
+++ 02-basics/projects/project05-data_inspector/inspector.c
@@ -19,24 +19,3 @@ int main()
     printf("\nStorage Check\nChar takes %lu byte(s)\nInt takes %lu byte(s)\n", sizeof(char), sizeof(int));
     return 0;
 }
-
-/*
-
-┌──(fiberlabs㉿kali)-[~/VSCode/c-course/02-basics/projects/project05-data_inspector]
-└─$ gcc inspector.c -Wall -Wextra -o inspector && ./inspector 
-enter a char value (e.g. @, X, h, etc.): ^
-enter a integer value (e.g. 1, 2, 3, etc.): 78
---- INSPECTION REPORT ---
-Symbol: ^ -> ASCII code: 94
-Next Symbol: _
-
-Integer: 78
-Hex View:4e
-Last Digit: 8
-
-Storage Check
-Char takes 1 byte(s)
-Int takes 4 byte(s)
-
-┌──(fiberlabs㉿kali)-[
-*/
