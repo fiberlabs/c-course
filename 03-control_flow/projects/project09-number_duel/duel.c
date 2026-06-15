2a5cd6963bb02e27a8ef034f5393e72b9ce5397a
fiberlabs <fiberlabs@protonmail.com>
2026-06-15 06:06:52 +0500

    Updated Project 9: Number Duel... it now has a while loop and randomness added.

--- 03-control_flow/projects/project09-number_duel/duel.c
+++ 03-control_flow/projects/project09-number_duel/duel.c
@@ -1,4 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL)); 
+
+    int secret_number = 1 + rand() % 10;
+    int user_input;
+
+    // printf("the secret number is: %d\n", secret_number); //just to check
+
+
+    while (user_input != secret_number)
+    {
+
+        printf("guess the secret number (bw 1-10): ");
+        scanf("%d", &user_input);
+
+        if (user_input < secret_number) {
+            printf("wrong... try a bigger number\n");
+            continue;
+        }
+        else if (user_input > secret_number) {
+            printf("wrong... try a smaller number\n");
+            continue;
+        }
+        else {
+            printf("you won!\n");
+            break;
+        }
+
+    }
+
+    return 0;
+}
+
+/* OLD CODE:
+#include <stdio.h>
 
 int main() 
 {
@@ -20,3 +59,5 @@ int main()
     return 0;
 
 }
+
+*/
